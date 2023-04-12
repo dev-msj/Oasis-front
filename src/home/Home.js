59f56f4300b23a6ddf41050c9d2c14c53998736c
dev-msj <msj944@naver.com>
2023-04-13 06:14:41 +0900

    feat(Home) : 종류별 추천 책 정보 받아오도록 변경

--- src/home/Home.js
+++ src/home/Home.js
@@ -2,15 +2,17 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Logout from "../login/Logout"
 import SuggestionList from "./section/SuggestionList";
+import { Radio } from "antd";
 
 const Home = () => {
 	const [Data, setData] = useState([]);
+	const [Type, setType] = useState("RECOMMEND");
 
 	useEffect(() => {
         axios.post(`http://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}/api/home/suggestion`,
 			JSON.stringify({
 				uid: window.sessionStorage.getItem("uid"),
-				suggestionType: "NEWBOOK"
+				suggestionType: Type
 			}), {
 				headers: {
 					"Content-Type" : "application/json"
@@ -30,11 +32,20 @@ const Home = () => {
 			console.log(err)
 			console.log(err.response)
 		});
-    }, []);
+    }, [Type]);
+
+	const onChange = value => {
+		setType(value.target.value);
+	}
 
     return (
 		<div style={{ width: "80%", height: "90vh", textAlign: "center" }}>
 			<Logout />
+			<Radio.Group defaultValue="RECOMMEND" onChange={onChange} buttonStyle="solid">
+				<Radio.Button value="RECOMMEND">추천</Radio.Button>
+				<Radio.Button value="NEWBOOK">신간</Radio.Button>
+				<Radio.Button value="BESTSELLER">베스트셀러</Radio.Button>
+			</Radio.Group>
 			<SuggestionList data={Data} />
 		</div>
 	);
