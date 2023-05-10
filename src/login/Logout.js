746e9eb0aac852eb5e94e3ecc55e65f2389fbb9d
dev-msj <msj944@naver.com>
2023-05-11 04:29:28 +0900

    refactor(Logout) : 세션 체크가 헤더에서 관리되므로 리로드만 하도록 변경

--- src/login/Logout.js
+++ src/login/Logout.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import { Button } from "antd";
 
 const Logout = () => {
-    const navigate = useNavigate();
-    
-    if (window.sessionStorage.getItem('uid') === null) {
-		navigate('/');
-	}
-
 	const handleLogout = () => {
 		window.sessionStorage.removeItem('uid');
 		window.sessionStorage.removeItem('word');
 		window.sessionStorage.removeItem('human');
-		navigate('/');
+		window.location.reload();
 	}
 
     return (
