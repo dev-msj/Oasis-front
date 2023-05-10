4e3336a0780b7ca16816bb8650a63a2064e17d2c
dev-msj <msj944@naver.com>
2023-05-11 04:30:08 +0900

    refactor(App) : Header, Footer 추가

--- src/App.js
+++ src/App.js
@@ -2,16 +2,30 @@ import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './home/Home';
 import Login from './login/Login';
+import { Layout } from 'antd';
+import { Content, Footer, Header } from 'antd/es/layout/layout';
+import ContentSelector from './footer/ContentSelector';
+import OasisHeader from './header/OasisHeader';
 
-const App = () => {
+const App = () => {  
   return (
     <Suspense fallback={(<div>Loading...</div>)}>
-      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "98vh", background: "#000000", overflow: "scroll" }}>
-        <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/home' element={<Home />} />
-        </Routes>
-      </div>
+      <Layout>
+        <Header style={{backgroundColor: 'black'}}>
+          <OasisHeader />
+        </Header>
+        
+        <Content style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "85vh", background: "#000000" }}>
+          <Routes>
+            <Route path='/' element={<Login />} />
+            <Route path='/home' element={<Home />} />
+          </Routes>
+        </Content>
+
+        <Footer style={{ display: "flex", justifyContent: "center", alignItems: "center", background: "#000000" }}>
+          <ContentSelector />
+        </Footer>
+      </Layout>
     </Suspense>
   );
 }
