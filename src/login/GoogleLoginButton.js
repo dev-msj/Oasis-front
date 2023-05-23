7d124ca9c5cdb136639cc662cc4caa61dfd4bc6f
dev-msj <msj944@naver.com>
2023-05-23 21:43:13 +0900

    refactor(GoogleLoginButton) : 직접 구글 oauth 토큰을 발급 받도록 수정

--- src/login/GoogleLoginButton.js
+++ src/login/GoogleLoginButton.js
@@ -1,24 +1,56 @@
 import React from 'react';
 import axios from 'axios';
-import { GoogleOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
 const GoogleLoginButton = () => {
-    const onClick = async () => {        
+    const navigate = useNavigate();
+
+    const script = document.createElement('script');
+    script.src = "https://accounts.google.com/gsi/client"
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+
+    window.loginCallback = async (data) => {
         const res = await axios.post(
-            `http://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}/api/auth/login/google`,
+            `http://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}/api/auth/login/google`, 
+            JSON.stringify({
+                token: data.credential
+            }), {
+                headers: {
+                    "Content-Type" : "application/json"
+                }, 
+                withCredentials: true
+            }
         );
 
-        if (res.data) {
-            window.location.href = `http://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}${res.data}`;
-        } else {
-            alert('Internal Server Error!');
+        if (res) {
+            console.log(res)
+            window.sessionStorage.setItem('uid', res.data.uid);
+            window.sessionStorage.setItem('joinState', res.data.joinState);
+            if (res.data.joinState === true) {
+                navigate('/home');
+            } else {
+                navigate('/join');
+            }
         }
-    };
-    
+    }
+
     return (
         <div style={{ textAlign: 'center' }}>
-            <Button onClick={onClick} icon={<GoogleOutlined />}>구글 로그인</Button>
+            <div id="g_id_onload"
+                data-client_id={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                data-callback="loginCallback"
+                data-auto_prompt="false">
+            </div>
+            <div className="g_id_signin"
+                data-type="standard"
+                data-size="large"
+                data-theme="outline"
+                data-text="sign_in_with"
+                data-shape="rectangular"
+                data-logo_alignment="left">
+            </div>
         </div>
     );
 }
