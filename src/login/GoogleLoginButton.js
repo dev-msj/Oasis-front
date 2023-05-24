09b98c447c478526076ca092c64c0f11f6072561
dev-msj <msj944@naver.com>
2023-05-24 13:43:41 +0900

    refactor(GoogleLogButton) : 버튼 관련 script를 useEffect에서 관리하도록 수정

--- src/login/GoogleLoginButton.js
+++ src/login/GoogleLoginButton.js
@@ -1,40 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const GoogleLoginButton = () => {
     const navigate = useNavigate();
 
-    const script = document.createElement('script');
-    script.src = "https://accounts.google.com/gsi/client"
-    script.async = true;
-    script.defer = true;
-    document.body.appendChild(script);
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = "https://accounts.google.com/gsi/client"
+        script.async = true;
+        script.defer = true;
+        document.body.appendChild(script);
 
-    window.loginCallback = async (data) => {
-        const res = await axios.post(
-            `http://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}/api/auth/login/google`, 
-            JSON.stringify({
-                token: data.credential
-            }), {
-                headers: {
-                    "Content-Type" : "application/json"
-                }, 
-                withCredentials: true
-            }
-        );
+        window.loginCallback = async (data) => {
+            const res = await axios.post(
+                `http://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}/api/auth/login/google`, 
+                JSON.stringify({
+                    token: data.credential
+                }), {
+                    headers: {
+                        "Content-Type" : "application/json"
+                    }, 
+                    withCredentials: true
+                }
+            );
 
-        if (res) {
-            console.log(res)
-            window.sessionStorage.setItem('uid', res.data.uid);
-            window.sessionStorage.setItem('joinState', res.data.joinState);
-            if (res.data.joinState === true) {
-                navigate('/home');
-            } else {
-                navigate('/join');
+            if (res) {
+                window.sessionStorage.setItem('uid', res.data.uid);
+                window.sessionStorage.setItem('joinState', res.data.joinState);
+                if (res.data.joinState === true) {
+                    navigate('/home');
+                } else {
+                    navigate('/join');
+                }
             }
         }
-    }
+    }, [navigate]);
 
     return (
         <div style={{ textAlign: 'center' }}>
