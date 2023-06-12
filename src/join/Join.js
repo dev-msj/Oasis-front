0875833720f2c4dedaedd3de3a2234c37dea9cb4
dev-msj <msj944@naver.com>
2023-06-13 06:46:29 +0900

    feat : 회원가입 화면 추가

--- src/join/Join.js
+++ src/join/Join.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Typography } from 'antd';
+import CreateUser from "./section/CreateUser";
+import CreateProfile from "./section/CreateProfile";
+
+const { Title } = Typography;
 
 const Join = () => {
+    const [join, setJoin] = useState(false);
 
-    return <div>회원가입 페이지</div>;
-}
+    return (
+        <div>
+            <Title style={{ textAlign: "center" }} level={2}><font color="#FFFFFF" style={{ fontFamily: "궁서" }}>Oasis</font></Title>
+            <div style={{ width: '350px' }}>
+                {
+                    join ? <CreateProfile /> : <CreateUser setJoin={setJoin} />
+                }
+            </div>
+        </div>
+    );
+};
 
 export default Join;
